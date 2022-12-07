94171765e2892ba3b254b750dfd15d29e056a925
Koti Talari <thalari.koti@gmail.com>
2022-12-07 22:53:53 +0530

    images and spelling mistakes

--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class ImageGalleryComponent {
  
   public items: any[] = [
-    { title: '', url: 'https://media.istockphoto.com/id/594020108/photo/university-of-florida.jpg?s=612x612&w=0&k=20&c=NnepSVaLV6-NXZG5O6VAB1g71d-4YywPEMeXPNy48-g=' },
-    { title: '', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdg_JwW1Zqo5kl-n_PJDvjOwxgNIZHXEON1w&usqp=CAU' },
-    { title: '', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_p3OMfg2sXFVZV2MAKe0JH4-Vr-NQNZ6UEA&usqp=CAU' }
+    { title: '', url: 'https://dz0zjhi21dz2t.cloudfront.net/media/79957/tour/1594062608498/1366_front.jpg'},
+    { title: '', url: 'http://media.bizj.us/view/img/1753691/florida-atlantic-university-fau*1200xx1500-844-0-18.jpg' },
+    { title: '', url: 'https://www.fau.edu/about/images/fau.jpg' },
+    { title: '', url: 'https://karandash.ua/storage/app/uploads/public/5e7/d2c/e5e/5e7d2ce5e8da7694143222.jpg' },
+    { title: '', url: 'http://graphics8.nytimes.com/images/2013/02/20/sports/STADIUM/STADIUM-superJumbo.jpg' },
+    { title: '', url: 'http://ww1.prweb.com/prfiles/2014/09/02/12139575/Florida_Atlantic_Owls_primary_logo.png' }
+    
+    
+    
   ];
 
   public width = '100%';
